185e4e3bf9c8515552b21eb88e7ed9244021ba80
valeriacora <42502649+valeriacora@users.noreply.github.com>
2018-08-19 04:07:54 -0300

    HTML5 ActivePresenter

---
+++ rlprez.js
@@ -0,0 +1,32 @@
+/*!
+ * ATOMI HTML5 Project Declarations
+ * Copyright (c) Atomi Systems, Inc. All rights reserved.
+ * https://atomisystems.com
+ * Version: 7.2.1
+ * Date: 2018.04.04 
+ */
+(function(AP) {
+	var d = {l:[[1920,1080]],params:{TocStyle:1,TocPos:0,IniTocState:0,ShowToolbar:true,TocWidth:250,ShowDuration:true,ShowMaxTocLevel:1,ShowSegments:true,IniVolume:80,IniCCShow:1,ShowLastBtn:false,DisplayMode:1,Navigation:true,ShowFirstBtn:false,ShowPrevBtn:false,ShowPlayBtn:true,ShowSpeedBtn:false,ShowNextBtn:false,ProgressColor:"rgb(255, 0, 0)",ShowVolumeBar:true,ShowVolumeBtn:true,ShowCCBtn:false,ShowDisplayModeBtn:true,ShowInfoBtn:false,ShowTocBtn:false,ShowPosition:true,ShowPrezLen:true,BgColor:"rgb(32, 32, 32)",FgColor:"rgb(255, 255, 255)"},homepage:"",projectName:"Video Creations ActivePresenter Test",e:null,copyright:"",appHomepage:"https://atomisystems.com",author:"Valeria",description:"",generator:"ActivePresenter",language:"pt-BR",mediaStats:[1,0,0,0,0,0,0,0,0],mpv:"7.2.5",wmd:"iVBORw0KGgoAAAANSUhEUgAAAR0AAABJCAYAAAAAAvIuAAAABHNCSVQICAgIfAhkiAAAAGdJREFUeJztwQENAAAAwqD3T20PBxQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJwaRWwAAWilvgAAAAAASUVORK5CYII=",options:{playerResource:"player/",opModes:15,testMaxTime:0,reportMethod:0,css:"rlprez.css",passCondition:3,reportFormat:0,resourceLocation:"resources/",hilightFocus:false,reportAddress:"",scormScorePercent:false,showUnmuteMsg:true,passConditionValue:100,autoplay:true,generatePreview:false,generateSeparatePackage:true,useWebmOgg:true,lms:false,fileNamePrefix:""},toc:[],pcs:[{i:-1,st:0,d:79082,h:[1080],tl:[[0,"Linha do tempo principal"]]},null],pools:[],responsive:false,slides:[{at:"Deslizar 1",i:10,tl:[[0,"Linha do tempo principal",[[1,[[400,79082,0,1],[1003,79082,0,1],[200,0,0,1],[1002,0,0,1],[1004,0,79082,1]]],[40,[[400,73000,0,1],[200,69000,0,1],[400,0,0,1]]]]]],n:"",h:[1080],de:"",w:1,b:{f:[1,4294967295]},d:79082,cc:[{i:1,mi:10001,m:15,k:4,md:79082,n:"Video Creations_1",ti:-1,s:[{w:[{r:[0,0,1920,1080]}],d:1,t:["\r\n"],i:0,p:0,n:"Normal",r:0,f:null}],f:169934848,e:{l:[[3,[[13,["http://www.google.com","_blank"]]]]],s:-1,f:8},v_w:1920,v_h:1080},{i:40,m:15,r:"10_40",k:11,n:"Clique do mouse_40",e:{l:[[4,[[13,["http://www.google.com","_blank"]]]]],m:1,v:[0],s:-1,f:9,a:3},ti:-1,s:[{l:[2,4280910013,321],w:[{r:[779,918,351,71]}],d:1,t:["\r\n","font-family:'Arial';font-size:18px;color:#000000;text-align:center;","middle"],i:0,p:0,n:"Normal",r:0,f:null,a:[0]}],f:35651584,t:1}],e:null}],textDir:"ltr",v:"7.2.5"},
+	f = [];
+	d.f = f;
+
+	/* xAPI configuration */
+	// ActivityID that is sent for the statement's object
+	d.xAPICourseId = "";
+	// Course name for the activity
+	d.xAPICourseName = {"pt-BR": ""};
+	// Course description for the activity
+	d.xAPICourseDescription = {"pt-BR": ""};
+	// Pre-configured LRSes that should receive data, added to what is included
+	// in the URL and/or passed to the constructor function.
+	//
+	// An array of objects where each object may have the following properties:
+	//
+	//    endpoint: (including trailing slash '/')
+	//    auth:
+	//    allowFail: (boolean, default true)
+	//    version: (string, defaults to high version supported by TinCanJS)
+	d.xAPIRecordStores = [];
+
+	AP.loadedPrezs.push(d);
+})(AtomiAP);
